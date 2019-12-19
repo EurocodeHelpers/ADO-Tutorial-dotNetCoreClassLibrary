13abb45b5aa11d4c82ed2fc26a780d4457375008
Peter Cox <eurocodehelpers@outlook.com>
2019-12-19 15:16:57 +0000

    Edited cwl msg.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureDevOpsConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Again!");
+        }
+    }
+}
