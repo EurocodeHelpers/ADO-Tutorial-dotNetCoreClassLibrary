2cab3d439e416dc7a9da4246975b505b4b9b3b22
Peter Cox <eurocodehelpers@outlook.com>
2019-12-26 18:35:02 +0000

    Initial Commit - added ConsoleApp, ClassLib and NUnit Project

---
+++ src/CalculatorLibrary/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CalculatorLibrary
+{
+    public static class CalculatorLibrary
+    {
+       
+        public static int AddTwoNumbers(int x1, int x2){
+            return x1+x2;
+        }
+
+        public static int SubtractTwoNumbers(int x1, int x2){
+            return x1-x2;
+        } 
+        
+    }
+}
---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int x = CalculatorLibrary.CalculatorLibrary.AddTwoNumbers(3, 5);
+            System.Console.WriteLine(x);
+        }
+
+
+    }
+}
