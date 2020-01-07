0ee2958ab3faa9084ddb8f17af877f60fbca180c
Peter Cox <eurocodehelpers@outlook.com>
2020-01-07 10:52:35 +0000

    Added calculator and unit testing.

---
+++ MottMacdonald.Core/Calculator.cs
@@ -0,0 +1,13 @@
+namespace MottMacdonald.Core
+{
+    public class Calculator
+    {
+        public int AddTwoNumbers(int x1, int x2){
+            return x1+x2; 
+        }
+
+        public int SubtractTwoNumbers(int x1, int x2){
+            return x1-x2; 
+        }
+    }
+}
